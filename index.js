4a98f4f3dc7ac298be3c112a05b3eff24b1a5c4d
Abhishek Jaiswal <abhishek_jaiswal@sislinfotech.com>
2021-11-08 16:10:30 +0530

    added default route

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ var app = express();
 app.use(express.json());
 
 app.use('/users', require('./routes/user'));
+app.use('/', (req, res) => {
+    return res.send("Hello user");
+});
 
 app.listen(3000);
