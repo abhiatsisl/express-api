e9b8423b753aa5a4127e0c2f6cb32cf85556d9db
Abhishek Jaiswal <abhishek_jaiswal@sislinfotech.com>
2021-11-30 12:35:32 +0530

    fix:mongo connection

--- db.js
+++ db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
 const connectToMongo = () => {
-     mongoose.connect('mongodb://localhost:27017/test', () => {
-      console.log('Connected successfully');
+  //    mongoose.connect('mongodb://localhost:27017/test', () => {
+  //     console.log('Connected successfully');
+  // });
+
+  mongoose.connect('mongodb+srv://abhishek_jaiswal:abhishek@cluster0.0ew82.mongodb.net/test?retryWrites=true&w=majority', () => {
+    console.log('Connected successfully');
   });
 }
 module.exports = connectToMongo;
